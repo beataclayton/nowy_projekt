01fb30b988cc36d103f62671a3e45584dda117db
beataclayton <beata_clayton@op.pl>
2019-12-14 15:54:56 +0100

    kolejne apki

--- stringi.py
+++ stringi.py
@@ -1,2 +1,11 @@
 def capitalize(txt):
     return txt.capitalize()
+
+def casefold(txt):
+    return txt.casefold()
+
+def center(txt):
+    return txt.center()
+
+def count(txt):
+    return txt.count()
